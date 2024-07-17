a10d0c62fda5acb3192bfbff58b4bbf8154a76e5
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-17 16:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    if (arr.length === 0) return 0;
+    arr.sort((a, b) => a - b);
+    let minCost = 0;
+    while (arr.length > 1) {
+        let first = arr.shift();
+        let second = arr.shift();
+        let cost = first + second;
+        minCost += cost;
+        arr.push(cost);
+        arr.sort((a, b) => a - b);
+    }
+    return minCost;
 }
-
-module.exports=mincost;
+module.exports = mincost;
